0558c270f40e1c55976ddeae3c280fab74da3db1
Todd Christensen <tchristensen@somethingdigital.com>
2016-05-30 12:03:03 -0700

    Preload category child data in URL rewrite index.
    This cuts back on individual db loads for stores with large category
    trees.  With such a site, this reduces total category URL rewrite index
    time by 40%.

---
+++ app/code/community/SomethingDigital/EnterpriseIndexPerf/Model/Url/Rewrite/Category/Refresh/Changelog.php
@@ -0,0 +1,7 @@
+<?php
+
+class SomethingDigital_EnterpriseIndexPerf_Model_Url_Rewrite_Category_Refresh_Changelog
+    extends Enterprise_Catalog_Model_Index_Action_Url_Rewrite_Category_Refresh_Changelog
+{
+    use SomethingDigital_EnterpriseIndexPerf_Trait_Url_CategoryRewriteCache;
+}
